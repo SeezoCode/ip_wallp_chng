5fd234db0814338922c51c874a721ab35ebc9f65
David Stoček <57591015+STOKYS@users.noreply.github.com>
2021-12-07 09:54:20 +0100

    New functional versions

--- script_ubuntu_mate.sh
+++ script_ubuntu_mate.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-### NOT TESTED ON MINT YET!!!
 ip=`ifconfig | grep 192.168 | awk '{print $2}'`
 convert template.png -gravity North -pointsize 108 -annotate +280+640 "$ip" wallpaper.png
-getdir=`pwd`
-dir="file://$getdir/wallpaper.png"
-gsettings set org.mate.desktop.background picture-filename $dir
+getdir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+dir="$getdir/wallpaper.png"
+dconf write /org/mate/desktop/background/picture-filename "'$dir'"
+gsettings set org.gnome.desktop.background picture-uri $dir
+dconf write /org/gnome/desktop/background/picture-filename "'$dir'"
+
+# Funguje na pocitacich v SIT učebně
