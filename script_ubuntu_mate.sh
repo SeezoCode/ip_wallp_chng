5d757ca09a5900caf299ed1f6c602618dbc6b779
Filip <47699264+SeezoCode@users.noreply.github.com>
2021-12-13 11:06:20 +0100

    Update script_ubuntu_mate.sh

--- script_ubuntu_mate.sh
+++ script_ubuntu_mate.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-ip=`ifconfig | grep 192.168 | awk '{print $2}'`
-convert template.png -gravity North -pointsize 108 -annotate +280+640 "$ip" wallpaper.png
-getdir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-dir="$getdir/wallpaper.png"
+
+dir="/home/student/wallpaper.jpg"
 dconf write /org/mate/desktop/background/picture-filename "'$dir'"
 gsettings set org.gnome.desktop.background picture-uri $dir
 dconf write /org/gnome/desktop/background/picture-filename "'$dir'"
